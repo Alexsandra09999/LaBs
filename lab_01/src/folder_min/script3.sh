28821869d858f7dfc11d522bad1a48010cb4023d
Alexsandra09999 <113517781+Alexsandra09999@users.noreply.github.com>
2025-04-06 19:49:05 +0300

    s

--- lab_01/src/folder_min/script3.sh
+++ lab_01/src/folder_min/script3.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-output_file="/usr/local/folder_max/output.log"
-
-# Проверяем и создаём файл, если нужно
-[ -d "/usr/local/folder_max" ] || sudo mkdir -p "/usr/local/folder_max"
-[ -f "$output_file" ] || sudo touch "$output_file"
-sudo chown $USER "$output_file"
-
-# Записываем дату
-date >> "$output_file"
+date >> /usr/local/folder_max/output.log
